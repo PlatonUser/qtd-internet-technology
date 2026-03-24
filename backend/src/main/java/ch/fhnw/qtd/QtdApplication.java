072c30432c916ee394b73a08e667b29c117a695a
Platon Pashkevych <91381708+PlatonUser@users.noreply.github.com>
2026-03-24 20:45:18 +0000

    spring boot set up

---
+++ backend/src/main/java/ch/fhnw/qtd/QtdApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.qtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtdApplication.class, args);
+	}
+
+}
